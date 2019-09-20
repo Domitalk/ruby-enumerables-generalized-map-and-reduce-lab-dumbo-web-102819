2896d3063f4a2acb4aa5349ced6a04562a6f8ed5
Dominic Chu <dominic.m.chu@gmail.com>
2019-09-20 03:21:01 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -9,14 +9,12 @@ def map(array)
 end
 
 def reduce(source_array, starting_point = 0)
-  i = 0
-
   if source_array.all?(Numeric) == true
     temp_value = starting_point
   else
     temp_value = true
   end
-
+  i = 0
   while i < source_array.length do
     temp_value = yield(temp_value, source_array[i])
     i += 1
